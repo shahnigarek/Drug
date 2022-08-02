056fc820ccd8496c86307ed87e5b0213bc336bf3
shahnigarek <shahnigarek@code.edu.az>
2022-08-02 20:13:05 +0400

    admin controller created

--- Chemistry/Controllers/AdminController.cs
+++ Chemistry/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Entities;
+using DataAccess.Helpers;
+using DataAccess.Repositories.Implementations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,28 @@ using System.Threading.Tasks;
 
 namespace Manage.Controllers
 {
-    public class AdminController
-    {
-    }
+   
+    
+        public class AdminController
+        {
+            private AdminRepository _adminRepository;
+            public AdminController()
+            {
+                _adminRepository = new AdminRepository();
+
+            }
+            public Admin Autenticate()
+            {
+            AdminAuthentication: ConsoleHelper.WriteTextWithColor(ConsoleColor.Green, "Please enter username");
+                string username = Console.ReadLine();
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Green, "Please enter password");
+                string password = Console.ReadLine();
+
+                var admin = _adminRepository.Get(a => a.UserName.ToLower() == username.ToLower() && PasswordHasher.Decrypt(a.Password) == password);
+
+                return admin;
+
+            }
+        }
+    
 }
--- Chemistry/Program.cs
+++ Chemistry/Program.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using DataAccess.Helpers;
+using DataAccess.Repositories.Implementations;
+using Manage.Controllers;
+using System;
 namespace Manage
 {
     class Program
     {
         static void Main()
         {
+            AdminController _adminController = new AdminController();
+            AdminRepository _adminRepository = new AdminRepository();
+            ApothecaryController _apothecaryController = new ApothecaryController();
+            ApothecaryRepository _apothecaryRepository = new ApothecaryRepository();
+            MedicineController _medicineController = new MedicineController();
+            MedicineRepository _medicineRepository = new MedicineRepository();
+            OwnerController _ownerController = new OwnerController();
+            OwnerRepository _ownerRepository = new OwnerRepository();
+            PharmacyController _pharmacyController = new PharmacyController();
+            PharmacyRepository _pharmacyRepository = new PharmacyRepository();
 
+        Authentication: var admin = _adminController.Autenticate();
+
+
+            if (admin != null)
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Green, $"Welcome , {admin.UserName}");
+                Console.WriteLine();
+
+                while (true)
+                {
+
+                }
+            }
         }
     }
 }
