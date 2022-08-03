7a86c3c520a6d7b4054a2610f2e4620269d90acb
shahnigarek <shahnigarek@code.edu.az>
2022-08-03 14:08:06 +0400

    owner repository created

--- DataAccess/Repositories/Implementations/OwnerRepository.cs
+++ DataAccess/Repositories/Implementations/OwnerRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Entities;
+using DataAccess.Contexts;
+using DataAccess.Repositories.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,110 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Repositories.Implementations
 {
-    public class OwnerRepository
+    public class OwnerRepository : IRepository<Owner>
     {
+        private static int id;
+
+        public Owner Create(Owner entity)
+        {
+            id++;
+            entity.ID = id;
+            try
+            {
+                Dbcontexts.Owners.Add(entity);
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Something went wrong");
+            }
+            return entity;
+        }
+
+        public void Delete(Owner entity)
+        {
+            try
+            {
+
+                Dbcontexts.Owners.Remove(entity);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Something went wrong");
+            }
+        }
+
+        public Owner Get(Predicate<Owner> filter = null)
+        {
+            try
+            {
+                if (filter == null)
+                {
+                    return Dbcontexts.Owners[0];
+                }
+                else
+                {
+                    return Dbcontexts.Owners.Find(filter);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Something went wrong");
+                return null;
+            }
+        }
+
+        public List<Owner> GetAll(Predicate<Owner> filter = null)
+        {
+            try
+            {
+                if (filter == null)
+                {
+                    return Dbcontexts.Owners;
+
+                }
+                else
+                {
+                    return Dbcontexts.Owners.FindAll(filter);
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong");
+                return null;
+            }
+        }
+
+        public void Update(Owner entity)
+        {
+            try
+            {
+                var owner = Dbcontexts.Owners.Find(a => a.ID == entity.ID);
+                if (owner != null)
+                {
+                    owner.ID = entity.ID;
+                    owner.Surname = entity.Surname;
+                    owner.Name = entity.Name;
+                }
+
+            }
+
+            catch (Exception)
+            {
+
+                Console.WriteLine("Something went wrong");
+            }
+        }
+        void IRepository<Owner>.Create(Owner entity)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
+                    
