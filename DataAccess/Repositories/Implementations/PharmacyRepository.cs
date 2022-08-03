2c2c56f59dda1b396ccaedbecbb0e6aceb627e45
shahnigarek <shahnigarek@code.edu.az>
2022-08-03 16:28:01 +0400

    pharmacy repository created

--- DataAccess/Repositories/Implementations/PharmacyRepository.cs
+++ DataAccess/Repositories/Implementations/PharmacyRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Entities;
+using DataAccess.Contexts;
+using DataAccess.Repositories.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,121 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Repositories.Implementations
 {
-   public class PharmacyRepository
+    public class PharmacyRepository : IRepository<Pharmacy>
     {
+        private static int id;
+        public Pharmacy Create(Pharmacy entity)
+        {
+            id++;
+            entity.ID = id;
+            try
+            {
+                Dbcontexts.Pharmacies.Add(entity);
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Something went wrong");
+            }
+            return entity;
+        }
+
+        public void Delete(Pharmacy entity)
+        {
+
+            try
+            {
+
+                Dbcontexts.Pharmacies.Remove(entity);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Something went wrong");
+            }
+        }
+        public Pharmacy Get(Predicate<Pharmacy> filter = null)
+        {
+            try
+            {
+                if (filter == null)
+                {
+                    return Dbcontexts.Pharmacies[0];
+                }
+                else
+                {
+                    return Dbcontexts.Pharmacies.Find(filter);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Something went wrong");
+                return null;
+            }
+        }
+        public List<Pharmacy> GetAll(Predicate<Pharmacy> filter = null)
+        {
+            try
+            {
+                if (filter == null)
+                {
+                    return Dbcontexts.Pharmacies;
+
+                }
+                else
+                {
+                    return Dbcontexts.Pharmacies.FindAll(filter);
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong");
+                return null;
+            }
+
+        }
+        public void Update(Pharmacy entity)
+        {
+
+            try
+            {
+                var pharmacy = Dbcontexts.Medicines.Find(a => a.ID == entity.ID);
+                if (pharmacy != null)
+                {
+
+                    pharmacy.ID = entity.ID;
+                    pharmacy.Name = entity.Name;
+
+
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Something went wrong");
+            }
+
+        }
+        void IRepository<Pharmacy>.Create(Pharmacy entity)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
